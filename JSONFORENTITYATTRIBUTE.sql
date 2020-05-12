08183a49d53cdebf2014b9953c20078ac7b439e2
Pragya Sanjana <pragyasanjana23@gmail.com>
2020-05-12 13:22:14 -0700

    Commiting a file

---
+++ JSONFORENTITYATTRIBUTE.sql
@@ -0,0 +1,135 @@
+@jsonStringForEntityAttributeMaster  = [{
+    "AttributeMaster" : 
+	[
+	 {
+			  "AttributeMasterID"       :    -9999		         
+              ,"AttributeName"          :   "A"
+              ,"AttributeCode"          :   "0"
+              ,"AttributeValue          :   "No"
+              ,"IsActive"	    		:   1
+              ,"IsDeleted"				:	0
+              ,"IsRTAAttribute"			: 	0
+              ,"ControlType"			:	3
+              ,"ReviewedBy"				: 	"ADMIN"
+              ,"LastModifiedDate"		: 	"5/12/2020"
+              ,"LastModifiedByUser"		:	"ADMIN"
+              ,"LastModifiedByMachine"	:	"SYSTEM"
+      },
+      {
+			  "AttributeMasterID"       :	-9998	         
+              ,"AttributeName"			:	"A"
+              ,"AttributeCode"			:	"1"
+              ,"AttributeValue"			:	"YES"
+              ,"IsActive"				:	  1
+              ,"IsDeleted"				:	0
+              ,"IsRTAAttribute"			:	0
+              ,"ControlType"			:	 3
+              ,"ReviewedBy"				:	"ADMIN"
+              ,"LastModifiedDate"		:	"5/12/2020"
+              ,"LastModifiedByUser"		:	"ADMIN"
+              ,"LastModifiedByMachine"	:	"SYSTEM"
+      },
+	  {
+			  "AttributeMasterID"       :	-9997      
+              ,"AttributeName"			:	"A"
+              ,"AttributeCode"			:	"2"
+              ,"AttributeValue"			:	"MAYBE"
+              ,"IsActive"				:	1
+              ,"IsDeleted"				:	0
+              ,"IsRTAAttribute"			:	0 
+              ,"ControlType"			:	3
+              ,"ReviewedBy"				:	"ADMIN"
+              ,"LastModifiedDate"		:	"5/12/2020"
+              ,"LastModifiedByUser"		:	"ADMIN"
+              ,"LastModifiedByMachine"	:	"SYSTEM"
+      }
+    ]
+  },
+  {
+    "AttributeMasterMapping" : 
+	[
+      {
+        "AttributeMasterID"		    : -9999
+        ,"EntityType"				:  0
+        ,"IsDefaultValue"			: 0
+        ,"IsPartOfDefaultSet"		: 0
+        ,"DisplayOrder"			    : 1
+        ,"IsMandatory"			    : 0
+        ,"IsValidateFASLockDate"	: 0
+      },
+        {
+        "AttributeMasterID"		    : -9998
+        ,"EntityType"				: 0
+        ,"IsDefaultValue"			: 0
+        ,"IsPartOfDefaultSet"		: 0
+        ,"DisplayOrder"				: 2
+        ,"IsMandatory"				: 0
+        ,"IsValidateFASLockDate"	: 0
+      },
+        {
+        "AttributeMasterID"		    : -9997
+        ,"EntityType"				: 0
+        ,"IsDefaultValue"			: 0
+        ,"IsPartOfDefaultSet"		: 0
+        ,"DisplayOrder"				: 3
+        ,"IsMandatory"				: 0
+        ,"IsValidateFASLockDate"	: 0
+      },
+        {
+        "AttributeMasterID"		    : -9999
+        ,"EntityType"				: 2
+        ,"IsDefaultValue"			: 0
+        ,"IsPartOfDefaultSet"		: 0
+        ,"DisplayOrder"				: 1
+        ,"IsMandatory"				: 0
+        ,"IsValidateFASLockDate"	: 0
+      },
+        {
+        "AttributeMasterID"		    : -9998
+        ,"EntityType"				: 2
+        ,"IsDefaultValue"			: 0
+        ,"IsPartOfDefaultSet"		: 0
+        ,"DisplayOrder"				: 2
+        ,"IsMandatory"				: 0
+        ,"IsValidateFASLockDate"	: 0
+      },
+        {
+        "AttributeMasterID"		    : -9997
+        ,"EntityType"				: 2
+        ,"IsDefaultValue"			: 0
+        ,"IsPartOfDefaultSet"		: 0
+        ,"DisplayOrder"				: 3
+        ,"IsMandatory"				: 0
+        ,"IsValidateFASLockDate"	: 0
+      },
+        {
+        "AttributeMasterID"		    : -9999
+        ,"EntityType"				: 7
+        ,"IsDefaultValue"			: 0
+        ,"IsPartOfDefaultSet"		: 0
+        ,"DisplayOrder"				: 1
+        ,"IsMandatory"				: 0
+        ,"IsValidateFASLockDate"	: 0
+      },
+        {
+        "AttributeMasterID"		    : -9998
+        ,"EntityType"				: 7
+        ,"IsDefaultValue"			: 0
+        ,"IsPartOfDefaultSet"		: 0
+        ,"DisplayOrder"				: 2
+        ,"IsMandatory"				: 0
+        ,"IsValidateFASLockDate"	: 0
+      },
+        {
+        "AttributeMasterID"		    : -9997
+        ,"EntityType"				: 7
+        ,"IsDefaultValue"			: 0
+        ,"IsPartOfDefaultSet"		: 0
+        ,"DisplayOrder"				: 3
+        ,"IsMandatory"				: 0
+        ,"IsValidateFASLockDate"	: 0
+      }
+      ]
+  }    
+]
+
